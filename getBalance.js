611510b5f3c3a0af18e31612e7c9da883212e913
Nicolas Saporiti <nicolas.saporiti@gmail.com>
2021-10-30 01:47:36 -0300

    first commit

---
+++ getBalance.js
@@ -0,0 +1,77 @@
+require("dotenv").config();
+const axios = require("axios");
+const url = "https://fintual.cl/api/goals";
+const user = process.env.USER_EMAIL;
+
+axios
+  .get(url, {
+    params: {
+      user_email: process.env.USER_EMAIL,
+      user_token: process.env.USER_TOKEN,
+    },
+  })
+  .then(async (res) => {
+    const { data } = await res.data;
+    const factor = 1;
+
+    function formatNumber(n) {
+      return new Intl.NumberFormat("es-ES").format(n).split(",")[0];
+    }
+
+    const goals = data.map((data, index) => {
+      const { name, goal_type, nav, profit, deposited } = data.attributes;
+      return {
+        goal: name,
+        type: goal_type,
+        nav: nav / factor,
+        profit: profit / factor,
+        deposited: deposited / factor,
+        profit_ratio: ((profit / deposited) * 100).toFixed(2) + " %",
+      };
+    });
+
+    const formatGoals = goals.map((data, index) => {
+      const { goal, type, nav, profit, deposited, profit_ratio } = data;
+      return {
+        goal: goal,
+        type: type,
+        nav: formatNumber(nav),
+        goal_profit: formatNumber(profit),
+        deposited: formatNumber(deposited),
+        profit_ratio: profit_ratio,
+      };
+    });
+
+    let sumNav = 0;
+    let sumProfit = 0;
+    let sumDeposited = 0;
+
+    for (let i = 0; i < goals.length; i++) {
+      sumNav += goals[i].nav;
+      sumProfit += goals[i].profit;
+      sumDeposited += goals[i].deposited;
+    }
+
+    console.log("\x1b[35m",`                                       
+                                                                                                             
+    _|_|_|_|  _|              _|                          _|                                          
+    _|            _|_|_|    _|_|_|_|  _|    _|    _|_|_|  _|        _|_|_|    _|_|    _|_|_|  _|_|    
+    _|_|_|    _|  _|    _|    _|      _|    _|  _|    _|  _|      _|        _|    _|  _|    _|    _|  
+    _|        _|  _|    _|    _|      _|    _|  _|    _|  _|      _|        _|    _|  _|    _|    _|  
+    _|        _|  _|    _|      _|_|    _|_|_|    _|_|_|  _|  _|    _|_|_|    _|_|    _|    _|    _|  
+                                                                                                      
+    `, "\x1b[35m")
+    console.log("\x1b[32m ____________________________________________________________________________________________________\x1b[32m", "\x1b[0m");
+    console.log(" ");
+    console.log("\x1b[32m Bienvenido \x1b[32m " + user + "\x1b[32m !!! Este es tu balance en FINTUAL a la fecha  \x1b[32m ", "\x1b[0m")
+    console.log("\x1b[32m ____________________________________________________________________________________________________\x1b[32m", "\x1b[0m");
+    console.table(formatGoals);
+    console.table({
+      sumNav: formatNumber(sumNav),
+      sumProfit: formatNumber(sumProfit),
+      sumDeposited: formatNumber(sumDeposited),
+      profit_ratio: ((sumProfit / sumDeposited) * 100).toFixed(2) + " %",
+    });
+
+  })
+  .catch(error => console.log(error.message));
